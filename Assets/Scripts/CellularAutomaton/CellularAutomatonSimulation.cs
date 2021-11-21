f46494bfcbbaef8cd260b91113fb5bc8e80608b1
erdeanmich <erdeanlinda@gmail.com>
2021-11-21 16:06:25 +0100

    Add iterations to CA

--- Assets/Scripts/CellularAutomaton/CellularAutomatonSimulation.cs
+++ Assets/Scripts/CellularAutomaton/CellularAutomatonSimulation.cs
@@ -13,7 +13,16 @@ namespace CellularAutomaton
 
         [SerializeField]
         [Range(0,100)]
-        private int floorRatio;
+        private int initialWallChance;
+
+        [SerializeField] 
+        private int caIterations;
+
+        [SerializeField] 
+        private int wallNeighborhoodThreshhold;
+
+        [SerializeField]
+        private int mooreNeighborHoodSize;
 
         [SerializeField]
         private CellularAutomatonVisualizer cellularAutomatonVisualizer;
@@ -22,8 +31,28 @@ namespace CellularAutomaton
     
         void Start()
         {
+            StartSimulation();
+        }
+
+        public void StartSimulation()
+        {
+            InitializeAutomaton();
+            for (int i = 0; i < caIterations; i++)
+            {
+                UpdateAutomaton();
+                VisualizeAutomaton();
+            }
+        }
+
+        private void InitializeAutomaton()
+        {
             RetrieveSeed();
             InitializeCellGrid();
+            VisualizeAutomaton();
+        }
+
+        private void VisualizeAutomaton()
+        {
             cellularAutomatonVisualizer.VisualizeAutomaton(cells);
         }
 
@@ -35,7 +64,7 @@ namespace CellularAutomaton
             {
                 for (int y = 0; y < gridSize; y++)
                 {
-                    cells[x, y] = Random.Range(0, 100) <= floorRatio ? 0 : 1;
+                    cells[x, y] = Random.Range(0, 100) <= initialWallChance ? 1 : 0;
                 }
             }
             
@@ -52,10 +81,57 @@ namespace CellularAutomaton
             }
         }
 
-        // Update is called once per frame
-        void Update()
+        private void UpdateAutomaton()
         {
-        
+            int[,] nextCells = new int[gridSize,gridSize];
+            for (int x = 0; x < gridSize; x++)
+            {
+                for (int y = 0; y < gridSize; y++)
+                {
+                    int wallCount = GetWallCountOfNeighborhood(x, y);
+                    CellType cellType = GetNextCellState(wallCount);
+                    nextCells[x, y] = (int) cellType;
+                }
+            }
+
+            cells = nextCells;
+        }
+
+        private CellType GetNextCellState(int neighborhoodWallCount)
+        {
+            return neighborhoodWallCount >= wallNeighborhoodThreshhold ? CellType.Wall : CellType.Floor;
+        }
+
+        private int GetWallCountOfNeighborhood(int x, int y)
+        {
+            int wallCount = 0;
+            for (int xOffset = x - mooreNeighborHoodSize; xOffset <= x + mooreNeighborHoodSize; xOffset++)
+            {
+                if (IsOffsetOutOfBounds(xOffset))
+                {
+                    continue;
+                }
+                
+                for (int yOffset = y - mooreNeighborHoodSize; yOffset <= y + mooreNeighborHoodSize; yOffset++)
+                {
+                    if (IsOffsetOutOfBounds(yOffset))
+                    {
+                        continue;
+                    }
+
+                    if (cells[xOffset, yOffset] == (int) CellType.Wall)
+                    {
+                        wallCount++;
+                    }
+                }
+            }
+
+            return wallCount;
+        }
+
+        private bool IsOffsetOutOfBounds(int offset)
+        {
+            return offset < 0 || offset > gridSize - 1;
         }
     }
 }
